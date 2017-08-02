29a43646e06fad94405dd29293fe7d0abb1446bb
Gurinder Singh <gurinder@orionesolutions.in>
2017-08-02 19:16:07 +0530

    new proxy server api skeleton

--- app/controllers.php
+++ app/controllers.php
@@ -9,3 +9,7 @@
 $container['App\Action\userService\UserController'] = function ($container) {
     return new App\Action\userService\UserController($container);
 };
+
+$container['App\Action\coreService\InitialisationController'] = function ($container) {
+    return new App\Action\coreService\InitialisationController($container);
+};
